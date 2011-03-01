47630babcb6f4a20774d4428aec2c79e981969b3
Cory Forsyth <cory.forsyth@gmail.com>
2011-03-01 17:11:44 -0500

    add js countdown timer

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -34,9 +34,15 @@ function startNumbers() {
 }
 
 var start_time;
+var max_mins = 0;
 var max_seconds = 10;
+var max_total = 60*max_mins + max_seconds;
 
 function startTimer() {
+  max_mins = parseInt($("#minutes").val(), 10);
+  max_seconds = parseInt($("#seconds").val(), 10);
+  max_total = max_mins * 60 + max_seconds;
+  console.log("max total: " + max_total);
   start_time = getSeconds();
   setTimeout(monitorTimer, 100);
 }
@@ -46,10 +52,29 @@ function getSeconds() {
   return d.getTime() / 1000;
 }
 
+function padNum(num, spaces, padding) {
+  padding = padding || "0";
+  spaces  = spaces  || 2;
+  num_str = num.toString();
+  while (num_str.length < spaces) {
+    num_str = padding + num_str;
+  }
+  return num_str;
+}
+
 function monitorTimer() {
   var elapsed = getSeconds() - start_time;
-  $("#timer").html(Math.round(elapsed));
-  if (elapsed < max_seconds) {
+  
+  var remaining = max_total - elapsed;
+  
+  var mins_remaining = parseInt(remaining / 60);
+  var secs_remaining = Math.round(remaining % 60);
+  secs_remaining = secs_remaining == 60 ? 0 : secs_remaining;
+  
+  $("#minutes").val(mins_remaining);
+  $("#seconds").val(padNum(Math.round(secs_remaining)));
+  
+  if (remaining > 0) {
     setTimeout(monitorTimer, 100);
   } else {
     startGuessing();

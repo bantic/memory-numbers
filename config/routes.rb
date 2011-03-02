a4177dedcb4bf16b4b5276c308275267e3a42d50
Cory Forsyth <cory.forsyth@gmail.com>
2011-03-02 16:03:23 -0500

    further manifest changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,14 @@ MemoryNumbers::Application.routes.draw do
   
   offline = Rack::Offline.configure do
     public_path = Pathname.new(Rails.public_path)
-    Dir[public_path.join("javascripts/*.js")].each do |file|
-      next if file =~ /all\.js/
-      cache Pathname.new(file).relative_path_from(public_path)
-    end
-    Dir[public_path.join("stylesheets/*.css")].each do |file|
-      next if file =~ /all\.css/
-      cache Pathname.new(file).relative_path_from(public_path)
-    end
     Dir[public_path.join("images/*")].each do |file|
       cache Pathname.new(file).relative_path_from(public_path)
     end
+  
+    cache "javascripts/rails.js"
+    cache "javascripts/application.js"
+    cache "javascripts/jquery.min.js"
+    cache "stylesheets/styles.css"
   
     network "/"
   end
